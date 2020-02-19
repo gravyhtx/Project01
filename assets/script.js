48b5ae0fe2777f326c3634515dd73f998fbf602f
Andres Long <andreslong95@gmail.com>
2020-02-19 11:53:28 -0600

    COORS err fix

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 console.log("Hopefully it works?");
-
+var songName = $("#song-name");
 var queryURL = "https://openwhyd.org/u/4d94501d1f78ac091dbc9b4d/playlist/10?format=json&limit=10000"
 
 $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    dataType: "jsonp"
 }).then(function(response){
-    console.log(response);
+    songName.text(response[0].name);
 })
