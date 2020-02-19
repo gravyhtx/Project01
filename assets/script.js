e61f3e6b68bee3e6a6b77fcf6a0872f38ae7f890
Andres Long <andreslong95@gmail.com>
2020-02-19 11:35:21 -0600

    Testing for COORS err

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 console.log("Hopefully it works?");
+
+var queryURL = "https://openwhyd.org/u/4d94501d1f78ac091dbc9b4d/playlist/10?format=json&limit=10000"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
