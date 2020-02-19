ad7556976980a6d487b0e2ea58ac0d1409532345
GravyHTX <godisgravy@gmail.com>
2020-02-19 12:48:20 -0600

    Updated Web Wireframe

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 console.log("Hopefully it works?");
+
+const element =  document.querySelector('.bounce')
+element.classList.add('animated', 'bounceInLeft')
+// const element2 =  document.querySelector('.yummy')
+// element2.classList.add('animated', 'bounceInLeft')
+            
+
+    // $(document).ready(function(){
+    //     $('.js--triggerAnimation').click(function(e){
+    //       e.preventDefault();
+    //       var anim = $('.js--animations').val();
+    //       testAnim(anim);
+    //     });
